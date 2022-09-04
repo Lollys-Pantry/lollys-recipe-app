601c6fe45f85eff683da57d6bfc7a653f6285081
Lea Ann Bradford <leaann@notch8.com>
2022-09-04 16:44:01 -0700

    get recipes from the api

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { ActivityIndicator, StyleSheet, FlatList, Text, View, Image } from 'react-native';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getRecipes = async () => {
+    try {
+      const response = await fetch('http://192.168.0.15:3000/api/v1/recipes');
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Recipes</Text>
       <Image
         style={styles.logo}
         source={require('./assets/LP_Retro_Logo.jpg')}
       />
       <StatusBar style="auto" />
+      {isLoading ? <ActivityIndicator size="large" color="#00ff00" /> : (
+        <>
+          <Text style={styles.header}>Recipes</Text>
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text style={styles.name}>{item.name}</Text>
+            )}
+          />
+        </>
+      )}
     </View>
   );
 }
@@ -20,16 +51,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 100,
   },
   header: {
     textAlign:'center',
     color: '#ff1493',
     fontWeight: 'bold',
     fontSize: 30,
+    marginBottom:16,
   },
   logo: {
     width: 200,
     height: 200,
+  },
+  name: {
+    fontSize:16,
   }
 });
